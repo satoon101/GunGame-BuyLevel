97610f43aa2f5e049707717bc73f941fc538be7e
Stephen Toon <satoon101@gmail.com>
2017-01-13 21:19:04 -0500

    Updated rules and added rules translation file.

--- addons/source-python/plugins/gungame/plugins/custom/gg_buy_level/rules.py
+++ addons/source-python/plugins/gungame/plugins/custom/gg_buy_level/rules.py
@@ -7,7 +7,6 @@
 # =============================================================================
 # GunGame
 from gungame.core.rules.instance import GunGameRules
-from gungame.core.rules.strings import rules_translations
 
 # Plugin
 from .info import info
@@ -17,3 +16,4 @@ from .info import info
 # >> RULES
 # =============================================================================
 buy_level_rules = GunGameRules(info.name)
+buy_level_rules.register_all_rules()
